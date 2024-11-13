e379e84c25fe54b4ccaa39630e9762077efc2c0d
Marcelo Alexandre Crispim Batista <mcrispim@c1r10s9.42lisboa.com>
2024-11-13 12:21:38 +0000

    test

--- 00_libf/ft_strnstr.c
+++ 00_libf/ft_strnstr.c
@@ -3,29 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mcrispim <mcrispim@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: mcrispim <mcrispim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 11:19:09 by mcrispim          #+#    #+#             */
-/*   Updated: 2024/11/13 11:19:58 by mcrispim         ###   ########.fr       */
+/*   Updated: 2024/11/13 11:53:49 by mcrispim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <stdio.h>
 
+char *ft_strnstr(const char *big, const char *little, size_t len)
+{
+    size_t i = 0;
+    size_t j;
+
+    if (!*little)
+        return (char *)big;
+
+    while (big[i] && i < len)
+    {
+        j = 0;
+        while (little[j] && big[i + j] && i + j < len && big[i + j] == little[j])
+        {
+            j++;
+        }
+        if (!little[j])
+            return (char *)&big[i];
+        i++;
+    }
+
+    return NULL;
+}
+
 int main(int c, char **v)
 {
-    (void) c;
-    (void) v;
+    (void)c;
+    (void)v;
 
     char big[] = "Hello World";
     char little[] = "ll";
     size_t len = 10;
 
-    char *result = strnstr(big,little,len);
+    char *result = strnstr(big, little, len);
 
     if (result != NULL)
-    printf("%s\n",result);
+        printf("%s\n", result);
     else
-    printf("NULL\n");
+        printf("NULL\n");
+
+    char *result2 = ft_strnstr(big, little, len);
+
+    if (result2 != NULL)
+        printf("%s\n", result2);
+    else
+        printf("NULL\n");
 }
