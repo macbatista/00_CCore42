c70dd744ca0518b33fc0cca3e6dad34c179eb445
Marcelo Alexandre Crispim Batista <mcrispim@c1r10s8.42lisboa.com>
2024-11-15 10:40:19 +0000

    15 of a milion

---
+++ 00_libf/ft_calloc.c
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+void *ft_calloc(size_t nmemb, size_t size)
+{
+void *ptr = malloc(nmemb * size);
+
+return(ptr);
+
+}
+
+
+/* int main(void)
+{
+    size_t size = 6;
+    size_t nmemb = 2;
+
+    char *ptr1 = calloc(nmemb, size);
+    if (ptr1 != NULL)
+    {
+        strncpy(ptr1, "TestTestTest", size - 1);
+        strncpy(ptr1 + size, "tESTtESTtEST", size - 1);
+        ptr1[size - 1] = '\0';
+        printf("String: %s\n", ptr1);
+    }
+    else
+    {
+        printf("Falhou");
+    }
+
+    size_t i = 0;
+    while (i < (size * nmemb))
+    {
+        if (ptr1[i] == '\0')
+        {
+            printf("[\\0]");
+        }
+        else
+        {
+            printf("%c", ptr1[i]);
+        }
+        i++;
+    }
+
+    char *ptr0 = ft_calloc(nmemb, size);
+    if (ptr0 != NULL)
+    {
+        strncpy(ptr0, "TestTestTest", size - 1);
+        strncpy(ptr0 + size, "tESTtESTtEST", size - 1);
+        ptr0[size - 1] = '\0';
+        printf("String: %s\n", ptr0);
+    }
+    else
+    {
+        printf("Falhou");
+    }
+
+    size_t j = 0;
+    while (j < (size * nmemb))
+    {
+        if (ptr0[j] == '\0')
+        {
+            printf("[\\0]");
+        }
+        else
+        {
+            printf("%c", ptr0[j]);
+        }
+        j++;
+    }
+
+} */
+
