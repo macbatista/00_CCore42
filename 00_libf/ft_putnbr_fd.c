56cf0bfc27c6ea75df80b287631e8bc519fc16e5
macbatista <m.a.c.batista@hotmail.com>
2024-11-15 13:07:29 +0000

    22 of a milion

--- 00_libf/ft_putnbr_fd.c
+++ 00_libf/ft_putnbr_fd.c
@@ -1,14 +1,26 @@
-#include <unistd.h>
-#include <limits.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcrispim <mcrispim@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 13:06:38 by mcrispim          #+#    #+#             */
+/*   Updated: 2024/11/15 13:06:51 by mcrispim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void ft_putchar_fd(char c, int fd)
+#include <limits.h>
+#include <unistd.h>
+
+/* void ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
-
-void ft_putnbr_fd(int n, int fd)
+ */
+void	ft_putnbr_fd(int n, int fd)
 {
-	unsigned int nbr;
+	unsigned int	nbr;
 
 	if (n < 0)
 	{
@@ -24,11 +36,11 @@ void ft_putnbr_fd(int n, int fd)
 	ft_putchar_fd((nbr % 10) + '0', fd);
 }
 
-int main(void)
+/* int main(void)
 {
 	ft_putnbr_fd(-1234, 1);
 	ft_putchar_fd('\n', 1);
 	ft_putnbr_fd(INT_MIN, 1);
 	ft_putchar_fd('\n', 1);
 	ft_putnbr_fd(INT_MAX, 1);
-}
+} */
