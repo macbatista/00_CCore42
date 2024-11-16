c949defdcf198885cf6fb96a6c922757c3bf2454
macbatista <m.a.c.batista@hotmail.com>
2024-11-16 13:32:19 +0000

    30_of_a_milion

--- 00_libf/ft_putnbr_fd.c
+++ 00_libf/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mcrispim <mcrispim@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 13:06:38 by mcrispim          #+#    #+#             */
-/*   Updated: 2024/11/16 13:14:27 by mcrispim         ###   ########.fr       */
+/*   Updated: 2024/11/16 13:18:50 by mcrispim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,22 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483648)
-	{
-		ft_putchar_fd('-', fd);
-		ft_putchar_fd('2', fd);
-		n = 147483648;
-	}
+	unsigned int nbr;
+
 	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		n = -n;
+		nbr = n * -1;
 	}
-	if (n > 9)
+	else 
+		nbr = n;
+	if (nbr > 9)
 	{
-		ft_putnbr_fd((n / 10), fd);
-		ft_putnbr_fd((n % 10), fd);
+		ft_putnbr_fd((nbr / 10), fd);
+		ft_putnbr_fd((nbr % 10), fd);
 	}
 	else
-		ft_putchar_fd((n + 48), fd);
+		ft_putchar_fd((nbr + 48), fd);
 }
 
 
