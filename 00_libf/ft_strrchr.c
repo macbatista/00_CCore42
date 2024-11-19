c0ef4cf75ec9ca1cfc0159d1262b1be61ce58935
Marcelo Alexandre Crispim Batista <mcrispim@c1r7s13.42lisboa.com>
2024-11-19 10:01:34 +0000

    38_of_a_milion

--- 00_libf/ft_strrchr.c
+++ 00_libf/ft_strrchr.c
@@ -6,36 +6,31 @@
 /*   By: mcrispim <mcrispim@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 09:04:34 by mcrispim          #+#    #+#             */
-/*   Updated: 2024/11/19 09:46:42 by mcrispim         ###   ########.fr       */
+/*   Updated: 2024/11/19 10:01:19 by mcrispim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-
-char *ft_strrchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
-	size_t len;
-	
+	size_t	len;
+
 	len = ft_strlen(s);
-		if (len <= 0)
-		return (NULL);
-	while (len >= 0)
-	{
-		if (s[len] == c)
-			return ((char*)&s[len]);
-		len --;
-	}
+	while (s[len] != (unsigned char)c && len > 0)
+		len--;
+	if (s[len] == (unsigned char)c)
+		return ((char *)s + len);
 	return (NULL);
-} 
-
-int main(void)
-{
-	printf("%s\n",strrchr("abcdabcdabcd", 'c'));
-	printf("%s\n",strrchr("", 'c'));
-	printf("%s\n",ft_strrchr("abcdabcdabcd", 'c'));
-	printf("%s\n",ft_strrchr("", 'c'));
 }
+
+/* int	main(void)
+{
+	printf("%s\n", strrchr("abcdabcdabcd", 'c'));
+	printf("%s\n", strrchr("", 'c'));
+	printf("%s\n", ft_strrchr("abcdabcdabcd", 'c'));
+	printf("%s\n", ft_strrchr("", 'c'));
+} */
