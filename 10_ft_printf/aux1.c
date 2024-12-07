ca87175bef626a5a631e99c09ed4f77c3b5378c7
macbatista <m.a.c.batista@hotmail.com>
2024-12-07 10:01:34 +0000

    Refactor ft_printf: rename header file and improve function formatting

--- 10_ft_printf/aux1.c
+++ 10_ft_printf/aux1.c
@@ -6,21 +6,21 @@
 /*   By: mcrispim <mcrispim@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 18:45:06 by mcrispim          #+#    #+#             */
-/*   Updated: 2024/12/06 18:54:09 by mcrispim         ###   ########.fr       */
+/*   Updated: 2024/12/07 09:56:54 by mcrispim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 
 int	ft_putchar(char c)
 {
 	write(1, &c, 1);
-    return (1);
+	return (1);
 }
 
 int	ft_putstr(char *s)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (s[i])
@@ -28,5 +28,5 @@ int	ft_putstr(char *s)
 		ft_putchar(s[i]);
 		i++;
 	}
-    return(i);
+	return (i);
 }
--- 10_ft_printf/main.c
+++ 10_ft_printf/main.c
@@ -6,11 +6,11 @@
 /*   By: mcrispim <mcrispim@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 16:47:37 by mcrispim          #+#    #+#             */
-/*   Updated: 2024/12/06 18:54:04 by mcrispim         ###   ########.fr       */
+/*   Updated: 2024/12/07 09:59:41 by mcrispim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 
 int	ft_printf(const char *n, ...)
 {
@@ -26,9 +26,9 @@ int	ft_printf(const char *n, ...)
 		if (n[i] == '%')
 		{
 			if (n[i + 1] == 'c')
-                rt += ft_putchar((char){va_arg(you, int)});
-            if (n[i + 1] == 's')
-			    rt += ft_putstr(va_arg(you, char *));
+				rt += ft_putchar((char){va_arg(you, int)});
+			if (n[i + 1] == 's')
+				rt += ft_putstr(va_arg(you, char *));
 			i += 2;
 		}
 		write(1, &n[i], 1);
@@ -38,17 +38,16 @@ int	ft_printf(const char *n, ...)
 	va_end(you);
 	return (rt);
 }
-//rt = ft_dkjasbd
-int main(void)
+
+// rt = ft_dkjasbd
+int	main(void)
 {
-    int rt;
-    
-    rt = ft_printf("123%c4567",'a');
-    printf("\n\nRt:%d\n\n",rt);
+	int	rt;
 
-    rt = printf("123%c4567",'a');
-    printf("\n\nRt:%d\n\n",rt);
-
-    rt = ft_printf("123%s4567","asdfghjkl");
-    printf("\n\nRt:%d\n\n",rt);
+	rt = ft_printf("123%c4567", 'a');
+	printf("\n\nRt:%d\n\n", rt);
+	rt = printf("123%c4567", 'a');
+	printf("\n\nRt:%d\n\n", rt);
+	rt = ft_printf("123%s4567", "asdfghjkl");
+	printf("\n\nRt:%d\n\n", rt);
 }
